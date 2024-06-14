ad1be959c0714155b7d3fee09424579f8776c0a4
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-06-14 15:04:55 +0330

    Upload Calculator project on Github

---
+++ README.md
@@ -0,0 +1,23 @@
+Project Name ===> Calculator
+
+Program Name ===> Calculator - Version 1
+
+Program Description ===> This Program Takes From The User One Of The 4 main operations Of mathematics And In The Next Step It Takes Numbers From The User To Apply The desired Option On The Numbers.
+
+Programming Language ===> Python
+
+Description Of Files:
+
+calculator_linux.py ===> For Linux 
+
+For Run ===> python3 calculator_linux.py
+
+calculator_windows.py ===> For Windows
+
+For Run ===> python calculator_windows.py
+
+Required packages for Python ===> pyfiglet / termcolor2 / os 
+
+pyfiglet ==> To install => pip install pyfiglet
+
+termcolor2 ==> To install => pip install termcolor2
---
+++ calculator_windows.py
@@ -0,0 +1,71 @@
+# Calculator
+
+# Library
+import os
+from pyfiglet import figlet_format
+from termcolor2 import colored
+ 
+# Clear Terminal
+os.system('cls')
+
+# Welcome
+print(colored('================================================', color='blue'))
+print(colored(figlet_format('Welcome'), color='cyan'))
+print(colored('================================================', color='blue'))
+print('                                            ')
+print(colored('Start Working With The Calculator', color='magenta'))
+print(colored('================================================', color='blue'))
+print('                                            ')
+
+# While
+while True:
+
+    # Value Input Option
+    user_op = input('''
+[+]
+[-]
+[*]
+[/]
+[out]
+Enter Option ===> ''')
+
+    # IF
+    if user_op == '+':
+        print('                                   ')
+        number1 = float(input('Enter Number1 ===> '))
+        number2 = float(input('Enter Number2 ===> '))
+        print('                                ')
+        print(f'Answer ===> {number1}+{number2}: {number1+number2}')
+
+    elif user_op == '-':
+        print('                                   ')
+        number1 = float(input('Enter Number1 ===> '))
+        number2 = float(input('Enter Number2 ===> '))
+        print('                                ')
+        print(f'Answer ===> {number1}-{number2}: {number1-number2}')
+
+    elif user_op == '*':
+        print('                                   ')
+        number1 = float(input('Enter Number1 ===> '))
+        number2 = float(input('Enter Number2 ===> '))
+        print('                                ')
+        print(f'Answer ===> {number1}*{number2}: {number1*number2}')
+
+    elif user_op == '/':
+        print('                                   ')
+        number1 = float(input('Enter Number1 ===> '))
+        number2 = float(input('Enter Number2 ===> '))
+        print('                                 ')
+        print(f'Answer ===> {number1}/{number2}: {number1/number2}')
+
+    elif user_op == 'out':
+        print(colored('=============================================', color='blue'))
+        print(colored(figlet_format('Good Luck'), color='cyan'))
+        break
+            
+    else:
+        print('                                     ')
+        print(colored('Choose Again', color='red'))
+
+# Create By Moein Heshmati
+# Finish
